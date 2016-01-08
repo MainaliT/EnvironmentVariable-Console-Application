897bfa18c1375206da5c47977e3e28cdff3d4a4a
Tek Mainali <parulmainali@gmail.com>
2016-01-07 19:59:59 -0500

    Environment Variable Console Application
    This project out put computer name, User name, Location and such.

---
+++ EnvironmentVariable/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Machine Name: {0} ", Environment.MachineName);
+            Console.WriteLine("CurrentDirectory: {0} ", Environment.CurrentDirectory);
+            Console.WriteLine("SystemDirectory: {0} ", Environment.SystemDirectory);
+            Console.WriteLine("UserDommain: {0} ", Environment.UserDomainName);
+            Console.WriteLine("UserName: {0} ", Environment.UserName);
+            Console.WriteLine("WorkingSet: {0} ", Environment.WorkingSet);
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
